3d2c8edac74a48a23e4934eac66e4f156c519a42
Matt Krueger <mkrueger@rstms.net>
2025-10-16 20:03:01 -0600

    Bump version: 0.1.14 → 0.1.15

--- cmd/root.go
+++ cmd/root.go
@@ -39,7 +39,7 @@ import (
 var cfgFile string
 
 var rootCmd = &cobra.Command{
-	Version: "0.1.14",
+	Version: "0.1.15",
 	Use:     "goon-template",
 	Short:   "golang project template tool",
 	Long: `
